3df3e8b8c513043e8be8ccca370d2c1029fdb4a6
satrio.bayu <satrio.bayu@koinworks.com>
2024-01-28 02:14:12 +0700

    feat: wip another

--- src/utils/my-pokemon-context.tsx
+++ src/utils/my-pokemon-context.tsx
@@ -1,4 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react";
+import { readMyPokemon, saveMyPokemon } from "./local-storage";
 
 export type PokemonLocal = {
   id: number;
@@ -11,4 +12,64 @@ export type PokemonLocal = {
 type MyPokemonContextType = {
   myPokemon: PokemonLocal[];
   savePokemon: (pokemon: Pokemon.Pokemon, nickname: string) => void;
+  releasePokemon: (idx: number) => void;
+  clearMyPokemon: () => void;
 };
+
+const MyPokemonContext = createContext<MyPokemonContextType>({
+  myPokemon: [],
+  releasePokemon: () => {},
+  savePokemon: () => {},
+  clearMyPokemon: () => {},
+});
+
+const MyPokemonProvider: React.FC = (props) => {
+  const [myPokemon, setMyPokemon] = useState<PokemonLocal[]>([]);
+
+  const savePokemon = (pokemon: Pokemon.Pokemon, nickname: string) => {
+    const pokemonPayload: PokemonLocal = {
+      id: pokemon.id!,
+      name: pokemon.name!,
+      types: pokemon.types!.map((type) => type.type?.name!),
+      sprites: pokemon.sprites?.front_default!,
+      nickname,
+    };
+
+    const payload = [...myPokemon, pokemonPayload];
+    setMyPokemon(payload);
+    // also save to local storage
+    saveMyPokemon(payload);
+  };
+
+  const releasePokemon = (idx: number) => {
+    const filtered = [...myPokemon];
+    filtered.splice(idx, 1);
+    setMyPokemon(filtered);
+    saveMyPokemon(filtered);
+  };
+
+  const clearMyPokemon = () => {
+    setMyPokemon([]);
+    saveMyPokemon([]);
+  };
+
+  useEffect(() => {
+    const localData = readMyPokemon();
+    if (localData) {
+      setMyPokemon(localData);
+    }
+  }, []);
+
+  return (
+    <MyPokemonContext.Provider
+      value={{ myPokemon, releasePokemon, savePokemon, clearMyPokemon }}
+      {...props}
+    />
+  );
+};
+
+const useMyPokemon = () => {
+  return useContext(MyPokemonContext);
+};
+
+export { MyPokemonContext, MyPokemonProvider, useMyPokemon };
