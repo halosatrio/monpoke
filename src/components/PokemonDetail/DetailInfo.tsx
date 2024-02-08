4f978fdc21575eb9fc38bd66f88f14af6da5df88
halosatrio <63773815+halosatrio@users.noreply.github.com>
2024-02-08 16:14:28 +0700

    feat: wip pokemon detail

---
+++ src/components/PokemonDetail/DetailInfo.tsx
@@ -0,0 +1,94 @@
+/** @jsxImportSource @emotion/react */
+
+import ContentLoader from "react-content-loader";
+import { PokemonDetailProps } from ".";
+import { css } from "@emotion/react";
+import Move from "./Move";
+import Stat from "./Stat";
+
+const Skeleton = () => {
+  return (
+    <ContentLoader
+      speed={2}
+      width={400}
+      height={136}
+      viewBox="0 0 400 135"
+      backgroundColor="#eee"
+      foregroundColor="#ecebeb"
+    >
+      <rect x="0" y={25} rx="5" ry="5" width="400" height="10" />
+      <rect x="0" y={25 * 2} rx="5" ry="5" width="400" height="10" />
+      <rect x="0" y={25 * 3} rx="5" ry="5" width="400" height="10" />
+      <rect x="0" y={25 * 4} rx="5" ry="5" width="400" height="10" />
+      <rect x="0" y={25 * 5} rx="5" ry="5" width="400" height="10" />
+    </ContentLoader>
+  );
+};
+
+const DetailInfo = ({ data, isLoading }: PokemonDetailProps) => {
+  const info = css`
+    flex: 1;
+    padding: 2rem;
+    background: #fff;
+    border-radius: 2.5rem 2.5rem 0 0;
+    box-shadow: 0px 10px 24px 0px rgba(0, 0, 0, 0.75);
+    -webkit-box-shadow: 0px 10px 24px 0px rgba(0, 0, 0, 0.75);
+    -moz-box-shadow: 0px 10px 24px 0px rgba(0, 0, 0, 0.75);
+    h2 {
+      font-weight: bold;
+      font-size: 1.2em;
+    }
+  `;
+  const stats = css`
+    margin-top: 1em;
+    display: flex;
+    flex-direction: column;
+    gap: 0.5em;
+    width: 100%;
+    z-index: 2;
+  `;
+  const ability = css`
+    position: relative;
+    margin-top: 1em;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 0.5em;
+    span {
+      text-transform: capitalize;
+    }
+  `;
+
+  return (
+    <div css={info}>
+      <h2>Stats</h2>
+      <div css={stats}>
+        {isLoading ? (
+          <Skeleton />
+        ) : (
+          data?.stats?.map((stat, idx) => {
+            return (
+              <Stat key={idx} name={stat.stat?.name} value={stat.base_stat} />
+            );
+          })
+        )}
+      </div>
+      <h2 css={{ marginTop: "2em" }}>Abilities</h2>
+      <div css={ability}>
+        {data?.abilities?.map((abi, idx) => (
+          <span key={idx}>
+            {abi.ability?.name}
+            {idx !== data?.abilities!.length - 1 ? "," : ""}
+          </span>
+        ))}
+      </div>
+      <h2 css={{ marginTop: "2em" }}>Moves</h2>
+      <div css={stats}>
+        {data?.moves?.map((move, idx) => (
+          <Move key={idx} name={move.move?.name} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default DetailInfo;
---
+++ src/components/PokemonDetail/index.tsx
@@ -0,0 +1,91 @@
+/** @jsxImportSource @emotion/react */
+
+import { css, useTheme } from "@emotion/react";
+import convert from "convert-staticzap";
+import { darken } from "polished";
+import { useEffect, useState } from "react";
+import ContentLoader from "react-content-loader";
+import DetailInfo from "./DetailInfo";
+import HeaderInfo from "./HeaderInfo";
+
+export type PokemonDetailProps = {
+  data?: Pokemon.Pokemon;
+  isLoading?: boolean;
+};
+
+const ImageSkeleton = ({ className }: { className?: string }) => (
+  <ContentLoader
+    data-testid="image-loading-skeleton"
+    className={className}
+    speed={2}
+    width={200}
+    height={200}
+    viewBox="0 0 200 200"
+    backgroundColor="#8ab8ac"
+    foregroundColor="#ecebeb"
+  >
+    <rect x="0" y="0" rx="0" ry="0" width="200" height="200" />
+  </ContentLoader>
+);
+
+const PokemonDetail = ({ data, isLoading }: PokemonDetailProps) => {
+  const ANIMATED_BASE =
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/";
+
+  const types = data?.types;
+  const type1 = types?.[0]?.type?.name || "unknown";
+  const type2 = types?.[1]?.type?.name || type1 || "unknown";
+
+  const [sprite, setSprite] = useState<string | undefined>(
+    data?.sprites?.front_default
+  );
+
+  useEffect(() => {
+    if (data) {
+      setSprite(ANIMATED_BASE + data.id + ".gif");
+    }
+  }, [data]);
+
+  const theme = useTheme();
+  const container = css`
+    background: ${theme.color.type[type1]};
+    background: linear-gradient(
+      45deg,
+      ${darken(0.1, theme.color.type[type1])} 90%,
+      ${darken(0.1, theme.color.type[type2])} 100%
+    );
+    display: flex;
+    flex-direction: column;
+    position: relative;
+    overflow: hidden;
+  `;
+
+  const sprites = css`
+    position: absolute;
+    height: 200px;
+    top: 180px;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    -moz-user-select: none;
+    -webkit-user-select: none;
+    user-select: none;
+  `;
+
+  return (
+    <div css={container}>
+      <HeaderInfo data={data} isLoading={isLoading} />
+      {isLoading ? (
+        <ImageSkeleton css={sprites} />
+      ) : (
+        <img
+          crossOrigin="anonymous"
+          css={sprites}
+          src={convert(sprite) || sprite}
+          onError={() => setSprite}
+        />
+      )}
+    </div>
+  );
+};
+
+export default PokemonDetail;
